7dc14acd0b25b505ff024ea7c0b3c3ac992600ca
ycolmena <ycolmena@emeal.nttdata.com>
2023-01-04 21:42:00 -0300

    Mostrar Sugerencias al escribir-autocomplete

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,17 +5,26 @@ import { PaisService } from '../../services/pais.service';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  
+  styles: [
+    `
+    li {
+        cursor: pointer;
+      }
+    `
+  ]
 })
 export class PorPaisComponent {
 
   termino: string = '';
   hayError: boolean = false;
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor(private paisService: PaisService) { }
 
   buscar(termino:string) {
+    this.mostrarSugerencias = false;
     this.termino = termino;
    
     this.paisService.buscarPais(termino)
@@ -34,7 +43,19 @@ export class PorPaisComponent {
 
 sugerencias( termino: string){
   this.hayError = false;
-  //Todo: crear sugerencias
+  this.termino = termino;
+  this.mostrarSugerencias = true;
+
+  this.paisService.buscarPais( termino )
+  .subscribe ( 
+    paises => this.paisesSugeridos = paises.splice(0,5),
+    (err) => this.paisesSugeridos = []    
+    );
+}
+
+  buscarSugerido( termino: string ) {
+    this.buscar( termino );
+   
 }
 
 }
